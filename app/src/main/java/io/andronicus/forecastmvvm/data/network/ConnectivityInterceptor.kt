70653c237759827ed6c2929a45384d04b491ba24
andronicus-kim <androkim.kim7@gmail.com>
2019-02-25 16:17:36 +0300

    connectivity interceptor

---
+++ app/src/main/java/io/andronicus/forecastmvvm/data/network/ConnectivityInterceptor.kt
@@ -0,0 +1,9 @@
+package io.andronicus.forecastmvvm.data.network
+
+import okhttp3.Interceptor
+
+/**
+ * Created by Andronicus on 2/25/2019.
+ */
+interface ConnectivityInterceptor : Interceptor{
+}
