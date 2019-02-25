f131591a3276e13227faaeeef69a7f7e7e76b960
andronicus-kim <androkim.kim7@gmail.com>
2019-02-25 14:39:17 +0300

    some changes

--- app/src/main/java/io/andronicus/forecastmvvm/data/ApixuWeatherApiService.kt
+++ app/src/main/java/io/andronicus/forecastmvvm/data/ApixuWeatherApiService.kt
@@ -1,6 +1,14 @@
 package io.andronicus.forecastmvvm.data
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import io.andronicus.forecastmvvm.data.response.CurrentWeatherResponse
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Created by Andronicus on 2/25/2019.
@@ -12,5 +20,33 @@ const val API_KEY = "829f083b43ad43f59f193522192502"
 interface ApixuWeatherApiService {
 
     @GET("current.json")
-    fun getCurrentWeather()
+    fun getCurrentWeather(@Query("q") location : String,
+                          @Query("lang") language : String = "en") : Deferred<CurrentWeatherResponse>
+
+    companion object {
+        operator fun invoke() : ApixuWeatherApiService{
+            val requestInterceptor = Interceptor{
+                chain -> val url = chain.request().url()
+                .newBuilder()
+                .addQueryParameter("key", API_KEY)
+                .build()
+
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+                return@Interceptor chain.proceed(request)
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(" http://api.apixu.com/v1/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+                .create(ApixuWeatherApiService::class.java)
+        }
+    }
 }
---
+++ app/src/main/java/io/andronicus/forecastmvvm/data/db/entity/Condition.kt
@@ -0,0 +1,7 @@
+package io.andronicus.forecastmvvm.data.db.entity
+
+data class Condition(
+            val text: String,
+            val icon: String,
+            val code: Int
+        )
