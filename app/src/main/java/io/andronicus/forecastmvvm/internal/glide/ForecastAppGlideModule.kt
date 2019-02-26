1cc243699d4b377dc246722d61fd108d0110a40f
andronicus-kim <androkim.kim7@gmail.com>
2019-02-26 14:50:46 +0300

    setting up glide

---
+++ app/src/main/java/io/andronicus/forecastmvvm/internal/glide/ForecastAppGlideModule.kt
@@ -0,0 +1,10 @@
+package io.andronicus.forecastmvvm.internal.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by Andronicus on 2/26/2019.
+ */
+@GlideModule
+class ForecastAppGlideModule : AppGlideModule()
